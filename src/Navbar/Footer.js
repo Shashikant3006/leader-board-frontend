9e5e7f2022b204f0465d05a6be96f50a49dba189
Shashikant <142254614+Shashikant3006@users.noreply.github.com>
2025-01-16 19:06:03 +0530

    Update Footer.js

--- src/Navbar/Footer.js
+++ src/Navbar/Footer.js
@@ -3,30 +3,30 @@ import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div
-      className="w-full flex justify-center items-center py-4 fixed bottom-0 left-0 bg-black"
-    >
-      <div className="flex items-center space-x-6">
-        <p className="text-white font-medium mr-4">
-          ©️ LeaderBoard
-        </p>
-        <NavLink
-          to="/about"
-          className={({ isActive }) => 
-            isActive ? "text-white font-medium" : "text-gray-100 hover:text-blue-400"
-          }
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="/help"
-          className={({ isActive }) => 
-            isActive ? "text-white font-medium" : "text-gray-100 hover:text-blue-400"
-          }
-        >
-          Help
-        </NavLink>
-      </div>
+    <div>
+    //   className="w-full flex justify-center items-center py-4 fixed bottom-0 left-0 bg-black"
+    // >
+    //   <div className="flex items-center space-x-6">
+    //     <p className="text-white font-medium mr-4">
+    //       ©️ LeaderBoard
+    //     </p>
+    //     <NavLink
+    //       to="/about"
+    //       className={({ isActive }) => 
+    //         isActive ? "text-white font-medium" : "text-gray-100 hover:text-blue-400"
+    //       }
+    //     >
+    //       About
+    //     </NavLink>
+    //     <NavLink
+    //       to="/help"
+    //       className={({ isActive }) => 
+    //         isActive ? "text-white font-medium" : "text-gray-100 hover:text-blue-400"
+    //       }
+    //     >
+    //       Help
+    //     </NavLink>
+    //   </div>
     </div>
   );
 };
